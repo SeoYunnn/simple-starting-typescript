c584515490438bfa606eab0ad8854ab89fd0d62b
seoyun-lee <20212025@o365.hanbat.ac.kr>
2023-11-30 22:05:30 +0900

    chap04 : First project

--- chap04_todo/src/index.ts
+++ chap04_todo/src/index.ts
@@ -44,9 +44,7 @@ function showCompleted(): Todo[] {
   return todoItems.filter(item => item.done);
 }
 
-// TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
 function addTwoTodoItems() {
-  // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
   addTodo({ id: 4, title: '야채호빵', done: false });
   addTodo({ id: 5, title: '맛있겠다', done: false });
 }
