a54d863145e824713ff77544999e213488e41875
seoyun-lee <20212025@o365.hanbat.ac.kr>
2023-12-01 09:17:13 +0900

    chap05 : interface

---
+++ chap05/src/index_signature.ts
@@ -0,0 +1,11 @@
+interface User {
+    [property: string]: string
+    id: string;
+    name: string;
+}
+
+const choonsik: User = {
+    id: '1',
+    name: '춘식',
+    address: '대전'
+};
---
+++ chap05/src/indexing.ts
@@ -0,0 +1,5 @@
+interface StringArray {
+    [index: number]: string;
+}
+
+const companies: StringArray = ['삼성', '카카오', '구글'];
